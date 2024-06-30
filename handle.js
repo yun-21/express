b08150b414ae45b140a25b77ac5051589b25d401
jiyun <ranji12@naver.com>
2024-06-30 18:08:45 +0900

    포춘쿠키 동적 콘텐츠 추가작성

--- handle.js
+++ handle.js
@@ -3,15 +3,29 @@ const {engine : test} = require('express-handlebars')
 const app = express()
 const port = 8080;
 
+//동적 콘텐츠
+const fortunes = [
+    "당신에게 오늘은 행운의 날입니다.",
+    "오늘은 물을 많이 마시는게 행운이 올라가겠네요",
+    "계단을 조심해야겠어요",
+    "사람 많은 곳을 가면 좋은 일이 생기겠네요",
+    "오늘은 독서를 하는게 좋겠어요"
+]
+
 //핸들바 뷰 엔진 설정
 app.engine('handlebars', test({
-    defaultLayout : 'logo',
+    defaultLayout : 'main',
 }))
 app.set('view engine', 'handlebars')
 
+//정적 콘텐츠
 app.use(express.static(__dirname+'/public'))
 
-app.get('/',(req,res)=> res.render('home'))
+
+app.get('/',(req,res)=> {
+    const random = fortunes[Math.floor(Math.random()*fortunes.length)]
+    res.render('home', {fortune:random})
+})
 
 app.get('/about',(req,res)=> res.render('about'))
 
