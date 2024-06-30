d5b7724a6fcef0753b1237fb12a73a9d103f72de
jiyun <ranji12@naver.com>
2024-06-30 16:54:52 +0900

    handlebars 함수가 아닌 객체로 불러옴 error

--- handle.js
+++ handle.js
@@ -1,9 +1,28 @@
 const express = require('express')
-const expressHandle = require('express-handlebars')
+const expressHandlebars = require('express-handlebars')
 const app = express()
+const port = 8080;
 
 //핸들바 뷰 엔진 설정
-app.engine('handlebars', expressHandle({
+app.engine('handlebars', expressHandlebars({
     defaultLayout : 'main',
 }))
 app.set('view engine', 'handlebars')
+
+app.get('/',(req,res)=> res.render('home'))
+
+app.get('/about',(req,res)=> res.render('about'))
+
+app.use((req,res)=>{
+    res.status(404)
+    res.render('404')
+})
+
+app.use((err,req,res,next)=>{
+    console.error(err.message)
+    res.status(500)
+    res.render('500')
+})
+app.listen(port, ()=>console.log(
+    `Express started on http://localhost:${port}; `+
+    `press Ctrl-C to terminate`))
