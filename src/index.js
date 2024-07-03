10ad1ebc0e36ff2f0b254d87800837711add355c
jiyun <ranji12@naver.com>
2024-07-04 00:42:48 +0900

    render공부중

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express()
 app.get('/',(req,res)=>{
     res.type('text/plain')
+    console.log(req.header)
     res.send("안녕")
+})
+app.get("/about", (req,res)=>{
+    res.render('about',{
+        message : 'Hello',
+        style : req.query.style,
+        userid : req.cookies.userid,
+        username : req.session.username
+    })
 })
 app.listen(8080)
