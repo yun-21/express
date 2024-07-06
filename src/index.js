cc1744ac6913ab3735b71a324ab0da89fadfefcd
jiyun <ranji12@naver.com>
2024-07-06 21:23:01 +0900

    render은 템플릿엔진(view)를 사용할떄 쓰는 메서드이다. 삭제

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express()
+// 로깅 미들웨어
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+  });
 app.get('/',(req,res)=>{
     res.type('text/plain')
     console.log(req.header)
     res.send("안녕")
 })
-app.get("/about", (req,res)=>{
-    res.render('about',{
-        message : 'Hello',
-        style : req.query.style,
-        userid : req.cookies.userid,
-        username : req.session.username
-    })
-})
+
 app.listen(8080)
