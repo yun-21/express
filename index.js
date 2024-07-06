dc8c9fd015bedcd8c1f095ee1fa99db1a966d4b5
jiyun <ranji12@naver.com>
2024-07-06 22:28:29 +0900

    특정 라우터 삭제

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const app = express()
 const path = require('path')
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 // 로깅 미들웨어
 app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-  });
-  app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+  console.log(`${req.method} ${req.url}`);
+  next();
 });
-
 
 app.listen(8080)
