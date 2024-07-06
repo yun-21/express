c543c4a24c5b609c36603c706807e2a9ec3e133e
jiyun <ranji12@naver.com>
2024-07-06 22:28:10 +0900

    정적 파일 작성 후 특정 라우터 작성

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express()
+const path = require('path')
+app.use(express.static(path.join(__dirname,'public')))
 // 로깅 미들웨어
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
   });
-app.get('/',(req,res)=>{
-    res.type('text/plain')
-    console.log(req.header)
-    res.send("안녕")
-})
+  app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
 
 app.listen(8080)
